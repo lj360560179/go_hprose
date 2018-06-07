f5bc4ffa84e3787239c54addc4775a63fea50d21
lj360560179 <360560179@qq.com>
2018-06-07 18:31:39 +0800

    go

--- locust/locust.go
+++ locust/locust.go
@@ -12,10 +12,6 @@ func foo(){
 	start := boomer.Now()
 	time.Sleep(100 * time.Millisecond)
 	elapsed := boomer.Now() - start
-	/*
-	Report your test result as a success, if you write it in locust, it will looks like this
-	events.request_success.fire(request_type="http", name="foo", response_time=100, response_length=10)
-	*/
 	boomer.Events.Publish("request_success", "http", "foo", elapsed, int64(10))
 }
 
@@ -23,13 +19,8 @@ func bar(){
 	start := boomer.Now()
 	time.Sleep(100 * time.Millisecond)
 	elapsed := boomer.Now() - start
-	/*
-	Report your test result as a failure, if you write it in locust, it will looks like this
-	events.request_failure.fire(request_type="udp", name="bar", response_time=100, exception=Exception("udp error"))
-	*/
 	boomer.Events.Publish("request_failure", "udp", "bar", elapsed, "udp error")
 }
-
 
 func main(){
 	task1 := &boomer.Task{
