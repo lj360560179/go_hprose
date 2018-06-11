671eb48980ce6c9b0c891e8453fedc9913a2138f
lj360560179 <360560179@qq.com>
2018-06-11 18:03:15 +0800

    go

--- client/push_client.go
+++ client/push_client.go
@@ -12,20 +12,25 @@ func (e *event) OnError(name string, err error) {
 	fmt.Printf("name: %s, err: %s\n", name, err.Error())
 }
 
+var result = make(chan interface{}, 1)
+
 func main() {
-	count:=0
-	for i:=0;i<20000;i++ {
+	for i:=0;i<10000;i++ {
 		go func() {
 			a ,_:= uuid.NewV1()
-
 			client := rpc.NewTCPClient("tcp4://172.16.1.102:10001/")
 			//client := rpc.NewTCPClient("tcp4://127.0.0.1:8888/")
 			client.SetEvent(&event{})
 			client.Subscribe("OTHER", a.String(), nil, func(ip string) {
-				count++
-				fmt.Println(ip)
+				result<-ip
 			})
 		}()
+	}
+
+	for v:= range result{
+		if v != "SUCCESS"{
+			fmt.Println(v)
+		}
 	}
 
 	time.Sleep(time.Second*3000000)
